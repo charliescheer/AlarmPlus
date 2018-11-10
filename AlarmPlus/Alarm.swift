cb3cda99e33429bd49dd9be7f6c45c76b0e071ea
charliescheer <charlie.scheer@automattic.com>
2018-11-10 16:13:22 -0700

    Added Alarm.swift file

---
+++ AlarmPlus/Alarm.swift
@@ -0,0 +1,13 @@
+//
+//  Alarm.swift
+//  AlarmPlus
+//
+//  Created by Charlie Scheer on 11/10/18.
+//  Copyright © 2018 Praxsis. All rights reserved.
+//
+
+import UIKit
+
+class Alarms: NSObject {
+    
+}
