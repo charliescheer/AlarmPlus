373e625eaed7f397f37b92fbc4d0f9bd2317f1f5
charliescheer <charlie.scheer@automattic.com>
2018-11-21 20:19:03 -0700

    Added the ability to be create Dates from time and date arrays in the Alarm class.  Also added the ability to create a dictionary of alarms

--- AlarmPlus/Models/Alarm.swift
+++ AlarmPlus/Models/Alarm.swift
@@ -12,9 +12,9 @@ class Alarms: NSObject {
     private var alarmTime: [Int] = []
     private var timesSnoozed: Int = 0
     private var repeatAlarm: Bool = false
-    private var daysActive: [String] = []
+    private var daysActive: [Int] = []
     private var alarmEnabled: Bool = true
-    private var nextAlarm: NSDate?
+    private var activeAlarms: [Int : Date] = [ : ]
     
     var snooze: Snooze?
     var alert: Alert?
@@ -57,49 +57,106 @@ class Alarms: NSObject {
         return self.timesSnoozed
     }
     
-    func addDayToAlarm(_ date: String) {
-        self.daysActive.append(date)
+    func addDayToAlarm(daysArray: [Int]) {
+        self.daysActive = daysArray
     }
     
-    func getAlarmDates() -> [String] {
+    func getAlarmDays() -> [Int] {
         return self.daysActive
     }
     
-    func removeDayFromAlarm(_ date: String){
-        for day in daysActive {
-            if date == day {
-                daysActive.removeAll{
-                    $0 == day
-                }
-            }
-        }
-    }
+    //    func removeDayFromAlarm(_ day: Int){
+    //        for days in daysActive {
+    //            if day == days {
+    //                daysActive.removeAll{
+    //                    $0 == days
+    //                }
+    //            }
+    //        }
+    //    }
     
-    func setAlarmTime(_ hour : Int, _ minute: Int) {
+    func setAlarmTime(hour : Int, minute: Int) {
         self.alarmTime.append(hour)
         self.alarmTime.append(minute)
     }
     
-    func getNextAlarm() -> NSDate? {
-        if let alarm = nextAlarm {
-            return alarm
-        }
-        return nil
+    func getAlarms() -> [Int : Date] {
+        return activeAlarms
     }
     
-    func setNextAlarm() {
+    func setActiveAlarams() {
+        //adds all of the different scheduled alarms to the activeAlarms array
+        for day in daysActive {
+            addDateToActiveAlarms(day)
+        }
+    }
+    
+    func addDateToActiveAlarms(_ day : Int) {
         var dateComponents = DateComponents()
+        let calendar = Calendar(identifier: .gregorian)
+        
+        dateComponents.calendar = calendar
+        
+        //set time for the new alarm
         if alarmTime.count == 1 {
-            dateComponents.hour = alarmTime[0]
-            dateComponents.minute = alarmTime[1]
+            dateComponents = setHoursAndMinutesForNewAlarm(dateComponents)
         } else {
             print("Time is out of range")
         }
         
+        //set date for the new alarm
+        let date = getNextDate(dayOfTheWeek: day)
+        let nextScheduleDateComponents = calendar.dateComponents(in: .current, from: date)
+        dateComponents.day = nextScheduleDateComponents.day
+        dateComponents.month = nextScheduleDateComponents.month
+        dateComponents.year = nextScheduleDateComponents.year
         
-        
+        activeAlarms[day] = dateComponents.date
     }
     
+    private func setHoursAndMinutesForNewAlarm(_ dateComponents: DateComponents) -> DateComponents{
+        var newDateComponents = dateComponents
+        newDateComponents.hour = alarmTime[0]
+        newDateComponents.minute = alarmTime[1]
+        
+        return newDateComponents
+    }
+    
+
+    private func getNextDate(dayOfTheWeek: Int) -> Date{
+        var newDate = Date()
+        
+        let calendar = Calendar(identifier: .gregorian)
+        let todaysDateCompenents = calendar.dateComponents(in: .current, from: newDate)
+        
+        if let todaysDayOfTheWeek = todaysDateCompenents.weekday {
+            print(dayOfTheWeek)
+            if dayOfTheWeek == todaysDayOfTheWeek {
+                //newDate is already set to today's date, make no changes
+            } else if dayOfTheWeek < todaysDayOfTheWeek {
+                // calculate the number of days to the next day of the week and return date
+                let daysTillSetDate = dayOfTheWeek - todaysDayOfTheWeek + 7
+                
+                print(daysTillSetDate)
+                if let modifiedDate = calendar.date(byAdding: .day, value: daysTillSetDate, to: newDate) {
+                    newDate = modifiedDate
+                }
+            } else {
+                //day is greater then todays day
+                //calculate the number of days to the next day and return that day of the week
+                let daysTillSetDate = dayOfTheWeek - todaysDayOfTheWeek
+                if let modifiedDate = calendar.date(byAdding: .day, value: daysTillSetDate, to: newDate) {
+                    newDate = modifiedDate
+                }
+            }
+        } else {
+            print("could not get today's day of the week")
+        }
+        
+        return newDate
+    }
+
+ 
 }
 
 //* Methods
@@ -125,3 +182,10 @@ class Alarms: NSObject {
 //* COMPLETE SetAlarmTime -> Void
 //* COMPLETE GetNextAlarm -> NSDate
 //* COMPLETE getNextAlarm
+
+
+extension Alarms {
+    enum constants {
+        static let daysOfTheWeek = [1 : "Sunday", 2 : "Monday", 3 : "Tuesday", 4 : "Wednesday", 5 : "Thursday", 6 : "Friday", 7 : "Saturday"]
+    }
+}
