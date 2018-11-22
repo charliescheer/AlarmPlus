88cab7ce8ea5c3f3b635039445ea901e3dff2474
charliescheer <charlie.scheer@automattic.com>
2018-11-21 21:17:04 -0700

    Initial properties and Methods for Snoozes

--- AlarmPlus/Models/Snooze.swift
+++ AlarmPlus/Models/Snooze.swift
@@ -9,5 +9,84 @@
 import UIKit
 
 class Snooze: NSObject {
+    private var snoozeLimitEnabled : Bool = false
+    private var snoozeLimitCount : Int = 0
+    private var snoozeLimitReached : Bool = false
+    private var challengeType : String = challenges.code
+    var challengeUI : UIView?
+    
+    func enableSnoozeLimit() {
+        snoozeLimitEnabled = true
+    }
+    
+    func disableSnoozeLimit() {
+        snoozeLimitEnabled = false
+    }
+    
+    func SnoozeLimitIsEnabled() -> Bool {
+        return snoozeLimitEnabled
+    }
+    
+    func increaseSnoozeLimitCount() {
+        snoozeLimitCount += 1
+    }
+    
+    func resetSnoozeLimitCount() {
+        snoozeLimitCount = 0
+    }
+    
+    func getSnoozeLimit() -> Int {
+        return snoozeLimitCount
+    }
+    
+    func snoozeLimitIsReached() {
+        snoozeLimitReached = true
+    }
+    
+    func snoozeLimitIsNotReached() {
+        snoozeLimitReached = false
+    }
+    
+    func isSnoozeLimitReached() -> Bool {
+        return snoozeLimitReached
+    }
+    
+    func setCallengeType(challenge : String) {
+        challengeType = challenge
+    }
+    
+    func getChallengeType() -> String {
+        return challengeType
+    }
+    
+    func displayChallenge() {
+        //display the UIView for the selected Challenge.
+        //Challenge functions should be built here?  Maybe in an extension?
+    }
+    
     
 }
+
+extension Snooze {
+    enum challenges {
+        static let walk = "walk"
+        static let math = "math"
+        static let code = "code"
+    }
+}
+
+//Snooze
+//
+//* Methods
+//COMPLETE ActivateSnoozeLimit -> Void
+//COMPLETE DeactivateSnoozeLimit -> Void
+//COMPLETE GetSnoozeLimitEnabledBool -> Bool
+//COMPLETE SetSnoozeLimit -> Void
+//COMPLETE GetSnoozeLimit -> Int
+//COMPLETE GetSnoozeLimitReached -> Bool
+//COMPLETE snoozeLimitReached -> Void
+//COMPLETE resetSnoozeLimitReached -> Void
+//COMPLETE selectChallengeType -> Void
+//COMPLETE getChallengeType -> String
+//* displayChallenge -> Void
+//* getChallengeUIView -> UIView
