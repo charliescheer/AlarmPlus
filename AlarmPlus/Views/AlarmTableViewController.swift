8647b0a1ac6f47b5962aacb2ebedf96940eea707
charliescheer <charlie.scheer@automattic.com>
2018-11-28 15:52:22 -0700

    added the ability to get and save alarm arrays to user defaults. added the basic controls for tableview on AlarmTableViewController

--- AlarmPlus/Views/AlarmTableViewController.swift
+++ AlarmPlus/Views/AlarmTableViewController.swift
@@ -9,16 +9,58 @@
 import UIKit
 
 class AlarmTableViewController: UITableViewController {
+    var scheduledAlarmEvents : [Date] = []
     
-    var alarm = Alarm()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-    
+        getSavedAlarmsFromMemory()
+        setupView()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        saveSavedAlarmsToMemory()
+    }
+    
+    func setupView() {
+        view.backgroundColor = styles.backgroundColor
+    }
+    
+    func getSavedAlarmsFromMemory() {
+        let userDefaults = UserDefaults()
+        if let savedAlarms = userDefaults.array(forKey: defaults.saved) as? [Date] {
+            scheduledAlarmEvents = savedAlarms
+        }
+    }
+    
+    func saveSavedAlarmsToMemory() {
+        let userDefaults = UserDefaults()
+        userDefaults.array(forKey: defaults.saved)
+    }
+    
+}
 
+extension AlarmTableViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: constants.alarmCellIdentifier)
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return scheduledAlarmEvents.count
+    }
+    
+    
+}
 
+extension AlarmTableViewController {
+    enum defaults {
+        static let saved = "savedAlarms"
+    }
+    
+    enum constants {
+        static let alarmCellIdentifier = "alarmCell"
+    }
 }
 
---
+++ AlarmPlus/Views/Styling/StylingConstants.swift
@@ -0,0 +1,13 @@
+//
+//  StylingConstants.swift
+//  AlarmPlus
+//
+//  Created by Charlie Scheer on 11/28/18.
+//  Copyright © 2018 Praxsis. All rights reserved.
+//
+
+import UIKit
+
+enum styles {
+    static let backgroundColor = UIColor.gray
+}
