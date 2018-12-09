1a1d6eb6158c40d0e7ac95b96e01d0405894ee16
charliescheer <charlie.scheer@automattic.com>
2018-12-09 16:30:00 -0700

    Removed some code and files that were part of the alarm settings table view controller

--- AlarmPlus/Views/AlarmSettingsViewController.swift
+++ AlarmPlus/Views/AlarmSettingsViewController.swift
@@ -49,23 +49,6 @@ class AlarmSettingsViewController: UIViewController {
     }
 }
 
-//extension AlarmSettingsTableViewController {
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return constants.settingsArray.count
-//    }
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell  = UITableViewCell(style: .default, reuseIdentifier: "settingsCell")
-//
-//        cell.textLabel?.text = constants.settingsArray[indexPath.row]
-//
-//        return cell
-//    }
-//
-//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//
-//    }
-//}
 
 extension AlarmSettingsViewController: StoryboardLoadable {
     static var storyboardName: String {
